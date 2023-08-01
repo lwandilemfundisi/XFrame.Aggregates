9b8e32d9d47f1fc0efb79901e287c916701c4405
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-08-02 01:48:43 +0200

    added entities

---
+++ XFrame.Aggregates/Entities/Entity.cs
@@ -0,0 +1,21 @@
+﻿using XFrame.Ids;
+using XFrame.ValueObjects;
+
+namespace XFrame.Aggregates.Entities
+{
+    public class Entity<TIdentity> : ValueObject, IEntity<TIdentity>
+        where TIdentity : IIdentity
+    {
+        public TIdentity Id { get; set; }
+
+        public IIdentity GetIdentity()
+        {
+            return Id;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Id;
+        }
+    }
+}
---
+++ XFrame.Aggregates/Entities/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using XFrame.Ids;
+
+namespace XFrame.Aggregates.Entities
+{
+    public interface IEntity
+    {
+        IIdentity GetIdentity();
+    }
+
+    public interface IEntity<out TIdentity> : IEntity
+        where TIdentity : IIdentity
+    {
+        TIdentity Id { get; }
+    }
+}
