62af98c70bd8e20b19eb47bb699e497cb17e9a67
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-08-02 02:16:51 +0200

    save

--- XFrame.Aggregates/AggregateRoot.cs
+++ XFrame.Aggregates/AggregateRoot.cs
@@ -88,41 +88,5 @@ namespace XFrame.Aggregates
             _exists = true;
             return this;
         }
-
-        private class CommittedDomainEvent : ICommittedDomainEvent
-        {
-            public long GlobalSequenceNumber { get; set; }
-            public string AggregateId { get; set; }
-            public string AggregateName { private get; set; }
-            public string Data { get; set; }
-            public string Metadata { get; set; }
-            public int AggregateSequenceNumber { get; set; }
-
-            public override string ToString()
-            {
-                return new StringBuilder()
-                    .AppendLineFormat("{0} v{1} ==================================", AggregateName,
-                        AggregateSequenceNumber)
-                    .AppendLine(PrettifyJson(Metadata))
-                    .AppendLine("---------------------------------")
-                    .AppendLine(PrettifyJson(Data))
-                    .Append("---------------------------------")
-                    .ToString();
-            }
-
-            private static string PrettifyJson(string json)
-            {
-                try
-                {
-                    var obj = JsonConvert.DeserializeObject(json);
-                    var prettyJson = JsonConvert.SerializeObject(obj, Formatting.Indented);
-                    return prettyJson;
-                }
-                catch (Exception)
-                {
-                    return json;
-                }
-            }
-        }
     }
 }
